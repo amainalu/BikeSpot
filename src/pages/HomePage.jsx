aa5bebe4a119bee56b2dadadec22c36ad40f6884
GosiaMas <malgorzata.maslowska81@gmail.com>
2020-12-17 14:41:05 +0000

    with styled homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,43 @@
 function HomePage() {
   return (
-    <div className="App Main">
-      <h1>Welcome to BikeSpot</h1>
-      <br />
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-        consectetur iste architecto repellendus sit fuga, est mollitia quibusdam
-        cumque libero officia eos impedit quo fugiat explicabo eius quaerat
-        beatae nam.
-      </p>
+    <div>
+      <div className="HomePageSetUp">
+        <div>
+          <h1 className="titleHomepage">Assure your bike safety</h1>
+          <img
+            className="bikeIcon"
+            src="../../bike_700x575-removebg-preview.png"
+            alt="bike icon"
+            style={{ float: "left" }}
+          />
+          <h3 className="textNextBike">
+            BikeSpot is a private bike parking service, that improves your bike
+            journeys by looking after the most essential, your bike!
+          </h3>
+        </div>
+
+        <div>
+          <section className="HomePageSection">
+            <h4 className="lessSpaceBetween">Easy Signup</h4>
+            <p className="lessSpaceBetween">
+              Choose the parking Spots that you want,
+            </p>
+            <p className="lessSpaceBetween">you can change them anytime</p>
+          </section>
+
+          <section className="HomePageSection">
+            <h4 className="lessSpaceBetween">Secure</h4>
+            <p className="lessSpaceBetween">24/7 survailed parking service,</p>
+            <p className="lessSpaceBetween">Authomatic access at any time,</p>
+          </section>
+
+          <section className="HomePageSection">
+            <h4 className="lessSpaceBetween">Fees</h4>
+            <p className="lessSpaceBetween">Rent a Spot for just 15 euros,</p>
+            <p className="lessSpaceBetween">Change rented spot for free</p>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
